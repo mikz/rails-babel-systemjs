08bee7948ae8f6119386b58d0c1df54055f1e0cd
Michal Cichra <michal@o2h.cz>
2015-06-04 19:49:53 +0200

    initial commit

---
+++ app/assets/javascripts/config.js
@@ -0,0 +1,32 @@
+System.config({
+  "baseURL": "/assets/",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.4.7",
+    "babel-runtime": "npm:babel-runtime@5.4.7",
+    "core-js": "npm:core-js@0.9.14",
+    "jquery": "github:components/jquery@2.1.4",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:core-js@0.9.14": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
+  }
+});
+
---
+++ app/assets/javascripts/welcome.es6
@@ -0,0 +1,13 @@
+import jQuery from 'jquery';
+
+class Hola {
+    greet(){
+        console.log("HOla!");
+    }
+}
+
+var hola = new Hola();
+
+hola.greet();
+
+export default Hola;
